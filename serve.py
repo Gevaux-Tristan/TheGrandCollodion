170898e3b9bf7aa81a61b36511c9e703f4a8c33b
Tristan Gevaux <gevauxtristan@gmail.com>
2025-05-07 22:54:16 -0400

    Delete serve.py

--- serve.py
+++
@@ -1,7 +0,0 @@
-from livereload import Server, shell
- 
-server = Server()
-server.watch('*.html', shell('echo "HTML changed"'))
-server.watch('*.js', shell('echo "JS changed"'))
-server.watch('*.css', shell('echo "CSS changed"'))
-server.serve(root='.', port=8000) 
